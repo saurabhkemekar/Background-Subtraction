623544d6746b8ca37c93631d18c0f517bf91f872
saurabhkemekar <50518930+saurabhkemekar@users.noreply.github.com>
2019-06-19 12:21:13 +0530

    Create frame_diffrence.py

---
+++ frame_diffrence.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+import os
+
+def nothing(x):
+	pass
+
+def get_frame_diff(pre_frame,curr_frame):
+
+	diff1 = cv2.absdiff(curr_frame,pre_frame)
+	
+
+	return diff1
+
+
+cap = cv2.VideoCapture(0)
+cv2.namedWindow('trackbar')
+cv2.createTrackbar('threshold','trackbar',0,255,nothing)
+
+while True:
+
+	ret,pre_frame  = cap.read()
+	ret,curr_frame = cap.read()
+	
+    # converting the bgr image to gray scale image
+	pre_frame = cv2.cvtColor(pre_frame,cv2.COLOR_BGR2GRAY)
+	curr_frame = cv2.cvtColor(curr_frame,cv2.COLOR_BGR2GRAY)
+	
+	background_image = get_frame_diff(pre_frame,curr_frame)
+	val = cv2.getTrackbarPos('threshold','trackbar')
+	a = np.array([0],np.uint8)
+	b = np.array([255],np.uint8)
+
+	image = np.where(background_image>val , b ,a)
+
+	kernel = np.array((3,3),np.uint8)
+
+	image = cv2.erode(image,kernel,iterations= 1)
+
+	image = cv2.dilate(image,kernel,iterations =1)
+
+	image = cv2.bitwise_and(curr_frame,curr_frame,mask= image)
+	
+	cv2.imshow('numpy_subtract',image)
+	
+	cv2.imshow('background_image',background_image)
+
+	k = cv2.waitKey(5) & 0xFF
+
+	if k==1:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
